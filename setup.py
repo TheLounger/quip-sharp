4b39bb1ac1bdc86afaacaf4f8533761573c1017e
Lounger <4087076+TheLounger@users.noreply.github.com>
2023-12-28 23:22:55 +0100

    Clean up code

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from setuptools import setup, Extension, find_packages
-from torch.utils import cpp_extension
-import platform
 import os
+import platform
 import subprocess
 import torch
 
+from setuptools import setup, find_packages
+from torch.utils import cpp_extension
+
+
 def get_cuda_version(cuda_home=os.environ.get('CUDA_PATH', os.environ.get('CUDA_HOME', ''))):
-    if cuda_home == '' or not os.path.exists(os.path.join(cuda_home,"bin","nvcc.exe" if platform.system() == "Windows" else "nvcc")):
+    if cuda_home == '' or not os.path.exists(os.path.join(cuda_home, "bin", "nvcc.exe" if platform.system() == "Windows" else "nvcc")):
         return ''
-    version_str = subprocess.check_output([os.path.join(cuda_home,"bin","nvcc"),"--version"]).decode('utf-8')
+    version_str = subprocess.check_output([os.path.join(cuda_home, "bin", "nvcc"), "--version"]).decode('utf-8')
     idx = version_str.find("release")
     return version_str[idx+len("release "):idx+len("release ")+4]
-    
+
+
 CUDA_VERSION = "".join(get_cuda_version().split(".")) if not os.environ.get('ROCM_VERSION', False) else False
 ROCM_VERSION = os.environ.get('ROCM_VERSION', False) if torch.version.hip else False
 
@@ -53,4 +56,3 @@ setup(
     )],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
 )
-
